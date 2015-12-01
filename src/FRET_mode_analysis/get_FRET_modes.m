60c11f973e9cc4f73abb1763516ba5f72376800e
Matthew Berginski <matthew.berginski@gmail.com>
2015-12-01 13:23:01 -0500

    adding more command processing with Donor/Acceptor

--- src/FRET_mode_analysis/get_FRET_modes.m
+++ src/FRET_mode_analysis/get_FRET_modes.m
@@ -1,12 +1,28 @@
-function [pixel_vals,mode_vals,hist_modes] = get_FRET_modes(search_str)
+function [pixel_vals,mode_vals,hist_modes] = get_FRET_modes(search_folder,varargin)
 
-base_name = fileparts(search_str);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%Setup variables and parse command line
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+i_p = inputParser;
 
-file_set = dir(search_str);
+i_p.addRequired('search_folder',@(x)exist(x,'dir') == 7);
+i_p.addParameter('DPA_folder',0,@(x)x==1 || x==0);
+
+i_p.addParameter('debug',0,@(x)x==1 || x==0);
+
+i_p.parse(exp_folder,varargin{:});
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%Main Program
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+base_name = fileparts(search_folder);
+
+file_set = dir(search_folder);
 file_set = file_set(3:end);
 
 assert(not(isempty(file_set)),...
-       sprintf('Cant Find any Files to analyze: %s',search_str));
+       sprintf('Cant Find any Files to analyze: %s',search_folder));
 
 fprintf('Reading in %d images.\n', length(file_set));
 
