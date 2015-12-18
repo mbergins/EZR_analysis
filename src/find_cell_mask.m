a013e0a2be8d9ccfd01136c01bd007fc49558534
Matthew Berginski <matthew.berginski@gmail.com>
2015-12-18 10:20:36 -0500

    removing rosin thresh, simplifying code

--- src/find_cell_mask.m
+++ src/find_cell_mask.m
@@ -1,4 +1,4 @@
-function find_cell_mask_region_thresh(acceptor_file,varargin)
+function find_cell_mask(acceptor_file,varargin)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%Setup variables and parse command line
@@ -9,8 +9,8 @@ i_p.StructExpand = true;
 i_p.addRequired('acceptor_file',@(x)exist(x,'file') == 2);
 
 i_p.addParameter('min_cell_area',20000,@(x)isnumeric(x) && x > 0);
-i_p.addParameter('unimodal_correction',2,@(x)isnumeric(x) && x > 0);
-i_p.addParameter('min_region_intensity_average',1000,@(x)isnumeric(x) && x > 0);
+i_p.addParameter('unimodal_correction',3,@(x)isnumeric(x) && x > 0);
+i_p.addParameter('min_region_intensity_average',500,@(x)isnumeric(x) && x > 0);
 
 i_p.addParameter('debug',0,@(x)x==1 || x==0);
 
@@ -25,6 +25,7 @@ acceptor_image_norm = normalize_image(acceptor_image);
 %%Main Program
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%find regions to look for cells
 cell_regions = acceptor_image > 0;
 cell_regions = bwpropopen(cell_regions,'Area',i_p.Results.min_cell_area,...
     'connectivity',8);
@@ -36,10 +37,12 @@ cell_regions = ismember(cell_regions,...
     find([region_props.MeanIntensity] > i_p.Results.min_region_intensity_average));
 cell_regions = bwlabel(cell_regions,8);
 
-rosin_regions = zeros(size(cell_regions,1),size(cell_regions,2));
+cell_structure_type = zeros(size(acceptor_image,1),size(acceptor_image,2));
+cell_label = zeros(size(acceptor_image,1),size(acceptor_image,2));
 
 for cell_region_num = 1:max(cell_regions(:))
-    this_region = acceptor_image .* double(cell_regions == cell_region_num);
+    this_region_binary = cell_regions == cell_region_num;
+    this_region = acceptor_image .* double(this_region_binary);
     acceptor_non_zero = nonzeros(this_region(:));
     
     unimodal_thresh_sets = [];
@@ -49,18 +52,11 @@ for cell_region_num = 1:max(cell_regions(:))
         unimodal_thresh_sets = [unimodal_thresh_sets,hist_centers(RosinThresh(hist_peaks))]; %#ok<AGROW>
     end
     
-    rosin_high_thresh = this_region > mean(unimodal_thresh_sets)*8;
-    rosin_high_thresh = bwpropopen(rosin_high_thresh,'Area',i_p.Results.min_cell_area,...
-        'connectivity',4);
-    rosin_high_thresh = fill_small_holes(rosin_high_thresh,50);
-    rosin_regions(rosin_high_thresh) = 1;
-    
     %find regions which appear to be part of a cell
     primary_cell_body = this_region > mean(unimodal_thresh_sets)*i_p.Results.unimodal_correction;
     primary_cell_body = bwpropopen(primary_cell_body,'Area',i_p.Results.min_cell_area,...
         'connectivity',4);
     primary_cell_body = fill_small_holes(primary_cell_body,50);
-    rosin_regions(primary_cell_body & not(rosin_regions)) = 2;
     
     %find dimmer structures that are still above background
     high_passed_image = apply_high_pass_filter(this_region,15);
@@ -75,54 +71,35 @@ for cell_region_num = 1:max(cell_regions(:))
     %and pick out only those connected objects which touch the confident cell
     %body image
     cell_body_overlap_labels = nonzeros(unique(high_threshed_label.*primary_cell_body));
-    cell_region = primary_cell_body | ismember(high_threshed_label,cell_body_overlap_labels);
+    cell_body_overlap = ismember(high_threshed_label,cell_body_overlap_labels);
     
-    rosin_regions(cell_region & not(primary_cell_body)) = 3;
+    cell_structure_type(cell_body_overlap) = 2;
+    cell_structure_type(primary_cell_body) = 1;
+    
+    cell_label(cell_body_overlap | primary_cell_body) = 1;
 end
 
+cell_label = bwlabel(cell_label,8);
+
+thick_perims = thicken_perimeter(bwperim(cell_label),cell_label);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Visualizations
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-vis_label = uint16(bwperim(rosin_regions > 0));
-vis_label(bwperim(rosin_regions > 0 & rosin_regions <= 2)) = 2;
+vis_label = thick_perims;
 vis_label(acceptor_image == 0) = 3;
 
 cmap = [1,0,0;0,1,0;1,1,0];
 
 cell_vis_image = create_highlighted_image(acceptor_image_norm,vis_label,'color_map',cmap);
 
-cmap = [1,0,1;0,1,0;0,0,1];
-rosin_thresh_vis = create_highlighted_image(acceptor_image_norm,rosin_regions,'color_map',cmap,'mix_percent',0.25);
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % File Output
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 [folder,file] = fileparts(acceptor_file);
 
-imwrite_with_folder_creation(rosin_regions > 0,...
-    fullfile(folder,'..','cell_masks',[file '.png']));
-
-imwrite_with_folder_creation(rosin_thresh_vis,...
-    fullfile(folder,'..','rosin_thresh_vis',[file '.png']));
-
-imwrite_with_folder_creation(uint8(rosin_regions),...
-    fullfile(folder,'..','rosin_thresh',[file '.png']));
+imwrite_with_folder_creation(uint8(cell_label),...
+    fullfile(folder,'..','cell_label',[file '.png']));
 
 imwrite_with_folder_creation(cell_vis_image,...
     fullfile(folder,'..','cell_mask_vis',[file '.png']));
-%
-% if (i_p.Results.debug)
-%     addpath('../visualize_cell_features/');
-%
-%     acceptor_image_orig = double(imread(acceptor_file));
-%     mask_min_max = csvread(fullfile(fileparts(acceptor_file),filenames.raw_mask_min_max));
-%     acceptor_image_orig_norm = (acceptor_image_orig - min(mask_min_max))/range(mask_min_max);
-%
-%     edge_highlight = create_highlighted_image(acceptor_image_orig_norm,bwperim(threshed_mask),'color_map',[1,0,0]);
-%     [pathstr,name, ~] = fileparts(acceptor_file);
-%     out_file = fullfile(pathstr,[name,'_edge.png']);
-%
-%     imwrite(edge_highlight,out_file);
-% end
