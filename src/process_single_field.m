924401e1e2b02c1c13739cc4b53c8a2a35a2c7fc
Matthew Berginski <matthew.berginski@gmail.com>
2015-12-18 10:21:26 -0500

    modifications to deal with new pipeline

--- src/process_single_field.m
+++ src/process_single_field.m
@@ -23,40 +23,13 @@ addpath(genpath('image_processing_misc'));
 acceptor_folder = fullfile(exp_folder,'Acceptor');
 acceptor_files = file_search('.*',acceptor_folder);
 
-% for i = 1:length(acceptor_files)
-%     find_cell_mask_region_thresh(fullfile(acceptor_folder,acceptor_files{i}));
-% end
-
-FRET_folder = fullfile(exp_folder,'FRET');
-FRET_files = file_search('.*',FRET_folder);
-
-% %FRET Highlighting
-% for i = 1:length(acceptor_files)
-%     highlight_low_FRET(fullfile(acceptor_folder,acceptor_files{i}), ...
-%         fullfile(acceptor_folder,FRET_files{i}),...
-%         0.37);
-%     fprintf('Done with %d\n', i);
-% end
-
-%FRET Analysis
-cell_mask_folder = fullfile(exp_folder,'cell_masks');
-cell_mask_files = file_search('.*',cell_mask_folder);
-
-rosin_thresh_folder = fullfile(exp_folder,'rosin_thresh');
-rosin_thresh_files = file_search('.*',rosin_thresh_folder);
-
-assert(length(cell_mask_files) == length(FRET_files));
-rosin_means = [];
-for i = 1:length(cell_mask_files)
-    rosin_temp = measure_FRET_from_edge(fullfile(FRET_folder,FRET_files{i}),...
-        fullfile(cell_mask_folder,cell_mask_files{i}),...
-        fullfile(acceptor_folder,acceptor_files{i}),...
-        fullfile(rosin_thresh_folder,rosin_thresh_files{i}));
-    
-    rosin_means = [rosin_means; rosin_temp];
+for i = 1:length(acceptor_files)
+    find_cell_mask(fullfile(acceptor_folder,acceptor_files{i}));
 end
 
-csvwrite(fullfile(exp_folder,'rosin_means.csv'),rosin_means);
+gather_per_cell_props(exp_folder);
+
+process_images_with_mask(exp_folder);
 
 mins = toc(start_full)/60;
 
