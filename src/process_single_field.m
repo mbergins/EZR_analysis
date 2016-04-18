c24c788f06cd73189d3db8b50e967704aa6c1a0a
Matthew Berginski <matthew.berginski@gmail.com>
2016-04-18 11:44:03 -0400

    adding option to use individual thresholds for each image

--- src/process_single_field.m
+++ src/process_single_field.m
@@ -23,11 +23,24 @@ addpath(genpath('image_processing_misc'));
 acceptor_folder = fullfile(exp_folder,'Acceptor');
 acceptor_files = file_search('.*',acceptor_folder);
 
-for i = 1:length(acceptor_files)
-    find_cell_mask(fullfile(acceptor_folder,acceptor_files{i}));
+if (exist(fullfile(exp_folder,'cell_mask_thresh.csv'),'file'))
+    cell_mask_thresh = csvread(fullfile(exp_folder,'cell_mask_thresh.csv'));
+else
+    cell_mask_thresh = [];
 end
 
-gather_per_cell_props(exp_folder);
+for i = 1:length(acceptor_files)
+    if (~ exist('this_mask_thresh','var') || isempty(this_mask_thresh))
+        find_cell_mask(fullfile(acceptor_folder,acceptor_files{i}));
+        1;
+    else
+        this_mask_thresh = cell_mask_thresh(cell_mask_thresh(:,1)==i,2);
+        find_cell_mask(fullfile(acceptor_folder,acceptor_files{i}),...
+            'unimodal_correction',this_mask_thresh); 
+    end
+end
+
+% gather_per_cell_props(exp_folder);
 
 process_images_with_mask(exp_folder);
 
