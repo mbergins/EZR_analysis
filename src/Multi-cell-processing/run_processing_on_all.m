f7d9b118db0f2b0a1c6026790b6c32813a5f43c8
Matthew Berginski <matthew.berginski@gmail.com>
2016-07-21 12:45:17 -0400

    adding master script to run multi-cell processing over multiple files

---
+++ src/Multi-cell-processing/run_processing_on_all.m
@@ -0,0 +1,38 @@
+function run_processing_on_all(folder)
+% run_processing_on_folders - run processing steps on all subfolders in a
+% given folder
+%
+% Parameters:
+%   folder - the folder you want to search for subfolders to process
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%Setup variables and parse command line
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+i_p = inputParser;
+
+i_p.addRequired('folder',@(x)exist(x,'dir') == 7);
+
+i_p.parse(folder);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%Main Program
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+sub_folders = dir(folder);
+%toss the first two, always '.' and '..'
+sub_folders = sub_folders(3:end);
+
+for i = 1:length(sub_folders)
+    if (sub_folders(i).isdir)
+        fprintf('Working on %s (%d of %d).\n',sub_folders(i).name,i,length(sub_folders));
+        try
+            segment_cell_edges(fullfile(folder,sub_folders(i).name));
+            process_cell_edges(fullfile(folder,sub_folders(i).name));
+            build_visualizations(fullfile(folder,sub_folders(i).name));
+        end
+    end
+end
+
+system('notify-send ''Done with Processing''')
+
+end
