9348779e9b35b4675bde86f4c8da7001fbf7fe1a
Matthew Berginski <matthew.berginski@gmail.com>
2017-03-22 16:31:32 -0400

    adding removal of objects close to zero regions

--- src/Multi-cell-processing/segment_cell_edges.m
+++ src/Multi-cell-processing/segment_cell_edges.m
@@ -25,18 +25,33 @@ for i_num = 1:length(file_set.Acceptor)
     Acceptor_norm = normalize_image(imread(file_set.Acceptor{i_num}),...,
         'quantile',[0.05,0.99],'only_nonzero',1);
 
-    edge_mask = Acceptor > 500;
+    %Pick out regions where EZR objects might be present
+    cell_region = Acceptor > 100;
+    
+    %Also block regions around zeros, which result from overexposed
+    %regions. Drop the region around those overexposed areas using a filter
+    %of the same size as the high-pass 
+    cell_region = cell_region & ~imdilate(Acceptor == 0,strel('disk',15));
     
     Acc_high = apply_high_pass_filter(Acceptor,15);
-    edge_mask = edge_mask & Acc_high > 0.5*std(Acc_high(:));
     
-    edge_mask = bwpropopen(edge_mask,'Area',100,'connectivity',4);
+    edge_mask = Acc_high > 2*std(Acc_high(cell_region));
+    
+    edge_mask = bwpropopen(edge_mask,'Area',20,'connectivity',4);
     edge_mask = fill_small_holes(edge_mask,10);
     edge_mask = remove_edge_objects(edge_mask);
+    edge_mask = edge_mask & cell_region;
     
-    edge_mask_label = bwlabel(edge_mask);
+    edge_mask_label = uint16(watershed_min_size(Acceptor,edge_mask,10));
     
-    non_edge = Acceptor > 1000 & ~edge_mask;
+    non_edge = Acceptor > std(Acceptor(:)) & imdilate(edge_mask,strel('disk',50)) & ~edge_mask;
+    wide_non_edge = Acceptor > std(Acceptor(:)) & ...
+        imdilate(edge_mask,strel('disk',50)) & ...
+        ~imdilate(edge_mask,strel('disk',10));
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Visualization
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
     non_edge_hightlight = create_highlighted_image(Acceptor_norm,non_edge,'mix_percent',0.5);
     
     mask_highlight = create_highlighted_image(Acceptor_norm,edge_mask);
@@ -48,6 +63,9 @@ for i_num = 1:length(file_set.Acceptor)
     mkdir_no_err(fullfile(exp_folder,'edge_mask'));
     imwrite(edge_mask,fullfile(exp_folder,'edge_mask',sprintf('%02d.png',i_num)));
 
+    mkdir_no_err(fullfile(exp_folder,'edge_mask_label'));
+    imwrite(edge_mask_label,fullfile(exp_folder,'edge_mask_label',sprintf('%02d.png',i_num)));
+    
     mkdir_no_err(fullfile(exp_folder,'edge_mask_highlight'));
     imwrite(mask_highlight,fullfile(exp_folder,'edge_mask_highlight',sprintf('%02d.png',i_num)));
     
@@ -57,6 +75,9 @@ for i_num = 1:length(file_set.Acceptor)
     mkdir_no_err(fullfile(exp_folder,'non_edge_mask'));
     imwrite(non_edge,fullfile(exp_folder,'non_edge_mask',sprintf('%02d.png',i_num)));
 
+    mkdir_no_err(fullfile(exp_folder,'wide_non_edge_mask'));
+    imwrite(wide_non_edge,fullfile(exp_folder,'wide_non_edge_mask',sprintf('%02d.png',i_num)));
+    
     mkdir_no_err(fullfile(exp_folder,'non_edge_mask_highlight'));
     imwrite(non_edge_hightlight,fullfile(exp_folder,'non_edge_mask_highlight',sprintf('%02d.png',i_num)));
 end
