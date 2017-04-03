dd51989e7b5042e54c24f9b9f731ec18780a1aa8
Matthew Berginski <matthew.berginski@gmail.com>
2017-04-03 13:13:55 -0400

    fixing error with new label matrix output

--- src/Multi-cell-processing/build_visualizations.m
+++ src/Multi-cell-processing/build_visualizations.m
@@ -8,7 +8,7 @@ i_p = inputParser;
 i_p.addRequired('exp_folder',@(x)exist(x,'dir') == 7);
 
 i_p.addParameter('debug',0,@(x)x==1 || x==0);
-i_p.addParameter('Eff_limits',[0.20,0.30],@(x)isnumeric(x) & ...
+i_p.addParameter('Eff_limits',[0.20,0.35],@(x)isnumeric(x) & ...
     length(x) == 2 & x(1) < x(2));
 
 i_p.parse(exp_folder,varargin{:});
@@ -23,11 +23,17 @@ addpath(genpath('../shared'));
 file_set = get_filenames(exp_folder);
 
 c_map = [0,0,0;jet(255)];
-mkdir_no_err(fullfile(exp_folder,'visualizations'));
-imwrite(output_color_map(c_map),...
-    fullfile(exp_folder,'visualizations','scale_bar.png'));
+
+vis_folder = fullfile(exp_folder,'visualizations');
+
+mkdir_no_err(vis_folder);
+
 imwrite(output_color_map(c_map,'labels',i_p.Results.Eff_limits),...
-    fullfile(exp_folder,'visualizations','scale_bar_labels.png'));
+    fullfile(vis_folder,'scale_bar_labels.png'));
+
+imwrite(output_color_map(c_map),...
+    fullfile(vis_folder,'scale_bar.png'));
+
 
 for i_num = 1:length(file_set.Acceptor)
     Acceptor = imread(file_set.Acceptor{i_num});
@@ -39,20 +45,20 @@ for i_num = 1:length(file_set.Acceptor)
     edge_mask = imread(file_set.edge_mask{i_num});
     edge_mask_label = imread(file_set.edge_mask_label{i_num});
     
-    props = regionprops(edge_mask,Acceptor,'Area','MajorAxisLength',...
+    props = regionprops(edge_mask_label,Acceptor,'Area','MajorAxisLength',...
         'MinorAxisLength','MeanIntensity');
     
-    Eff_props = regionprops(edge_mask,Eff,'MeanIntensity');
-
+    Eff_props = regionprops(edge_mask_label,Eff,'MeanIntensity');
+    
     No_force_highlight = create_highlighted_image(Acceptor_norm,Eff > 0.27 & Acceptor > 1000);
-    imwrite(No_force_highlight,fullfile(exp_folder,'visualizations',...
-        sprintf('No_force_%02d.png',i_num)));
+    imwrite(No_force_highlight,...
+        fullfile(vis_folder,sprintf('No_force_%02d.png',i_num)));
     
     Eff_mean = make_mean_image(edge_mask_label,Eff_props,'MeanIntensity');
-    Eff_mean_color = colorize_image(Eff_mean,c_map, ...
+    Eff_mean_color = colorize_image(Eff_mean,c_map,...
         'normalization_limits',i_p.Results.Eff_limits);
-    imwrite(Eff_mean_color,fullfile(exp_folder,'visualizations',...
-        sprintf('Eff_mean_%02d.png',i_num)));
+    imwrite(Eff_mean_color,...
+        fullfile(vis_folder,sprintf('Eff_mean_%02d.png',i_num)));
 end
 
 end
