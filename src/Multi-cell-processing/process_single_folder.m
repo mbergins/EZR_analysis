6b1db53936e9eda97a04316ac5b3294c3ef2ff8f
HoffmanLab <bhoffmanlab@gmail.com>
2017-06-23 10:24:25 -0400

    script to process just a single folder

---
+++ src/Multi-cell-processing/process_single_folder.m
@@ -0,0 +1,26 @@
+function process_single_folder(folder)
+% process_single_folder - run processing steps on a single folder
+%
+% Parameters:
+%   folder - the folder you want to process
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%Setup variables and parse command line
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+i_p = inputParser;
+
+i_p.addRequired('folder',@(x)exist(x,'dir') == 7);
+
+i_p.parse(folder);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%Main Program
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+segment_cell_edges(folder);
+process_cell_edges(folder);
+build_visualizations(folder);
+
+system('notify-send ''Done with Processing''')
+
+end
